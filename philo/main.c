9557e7fb8c3887e0bb69c8a04450d82b59bc1ecb
Ignacio Churruca Fernandez De Tejada <igchurru@c4r7s2.42urduliz.com>
2025-01-28 17:10:39 +0100

    Started filling data structs

--- philo/main.c
+++ philo/main.c
@@ -6,11 +6,21 @@
 /*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 12:17:51 by igchurru          #+#    #+#             */
-/*   Updated: 2025/01/28 16:44:52 by igchurru         ###   ########.fr       */
+/*   Updated: 2025/01/28 17:05:16 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	ft_initlife(int argc, char **argv, t_life *life)
+{
+	life->number_of_philos = ft_atoi(argv[1]);
+	life->ttdie = ft_atoi(argv[2]);
+	life->tteat = ft_atoi(argv[3]);
+	life->ttsleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		life->must_eat = ft_atoi(argv[5]);
+}
 
 void	ft_initphilos(int ac, char **av, t_philo *philo, pthread_mutex_t *fork)
 {
@@ -23,20 +33,22 @@ void	ft_initphilos(int ac, char **av, t_philo *philo, pthread_mutex_t *fork)
 	{
 		philo[i].id = i;
 		philo[i].time_of_lm = ft_get_current_time();
+		i++;
 	}
-	
 }
 
 int	main(int argc, char **argv)
 {
 	t_philo			philo[MAXPHILOS];
+	t_life			life;
 	pthread_mutex_t	fork[MAXPHILOS];
 
 	if (ft_check_args(argc, argv))
 		return (1);
 	ft_memset(&philo, 0, sizeof(t_philo));
 	ft_memset(&fork, 0, sizeof(t_life));
-	ft_fill_philo_data(argc, argv, philo, fork);
+	ft_initphilos(argc, argv, philo, fork);
+	ft_initlife(argc, argv, &life);
 	return (0);
 }
 
