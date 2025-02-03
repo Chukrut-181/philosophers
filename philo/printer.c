fda689d3475b3ff38809748d9ce6f1d0ab851f5b
Ignacio Churruca Fernandez De Tejada <igchurru@c4r7s2.42urduliz.com>
2025-02-03 13:52:17 +0100

    Solved issue of continuing simulation when one philo died.

--- philo/printer.c
+++ philo/printer.c
@@ -6,7 +6,7 @@
 /*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 13:21:34 by igchurru          #+#    #+#             */
-/*   Updated: 2025/02/03 11:19:14 by igchurru         ###   ########.fr       */
+/*   Updated: 2025/02/03 13:51:01 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,18 @@
 void	ft_printer(t_philo *philo, int k)
 {
 	size_t	time;
+	t_life	*life;
 
+	life = philo->life;
 	pthread_mutex_lock(&philo->life->print);
 	time = (ft_get_current_time() - philo->life->start_time);
-	if (k == GOTFORK)
+	if (k == GOTFORK && life->status == CONTINUE)
 		printf("%zu: Philosopher %i has taken a fork\n", time, philo->id);
-	else if (k == EATING)
+	else if (k == EATING && life->status == CONTINUE)
 		printf("%zu: Philosopher %i is eating\n", time, philo->id);
-	else if (k == SLEEPING)
+	else if (k == SLEEPING && life->status == CONTINUE)
 		printf("%zu: Philosopher %i is sleeping\n", time, philo->id);
-	else if (k == THINKING)
+	else if (k == THINKING && life->status == CONTINUE)
 		printf("%zu: Philosopher %i is thinking\n", time, philo->id);
 	else if (k == DEAD)
 		printf("%zu: Philosopher %i is dead\n", time, philo->id);
