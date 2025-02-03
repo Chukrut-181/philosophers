e547d92a23e6596a10f61d4d8bd78fc9a80300e3
Ignacio Churruca Fernandez De Tejada <igchurru@c4r7s2.42urduliz.com>
2025-02-03 16:05:57 +0100

    Corrected ft_printer to match subject.

--- philo/printer.c
+++ philo/printer.c
@@ -6,7 +6,7 @@
 /*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 13:21:34 by igchurru          #+#    #+#             */
-/*   Updated: 2025/02/03 15:31:40 by igchurru         ###   ########.fr       */
+/*   Updated: 2025/02/03 16:05:21 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,14 +35,14 @@ void	ft_printer(t_philo *philo, int k)
 	pthread_mutex_lock(&life->print);
 	time = (ft_get_current_time() - life->start_time);
 	if (k == GOTFORK && life->status == CONTINUE)
-		printf("%zu: Philosopher %i has taken a fork\n", time, philo->id);
+		printf("%zu: %i has taken a fork\n", time, philo->id);
 	else if (k == EATING && life->status == CONTINUE)
-		printf("%zu: Philosopher %i is eating\n", time, philo->id);
+		printf("%zu: %i is eating\n", time, philo->id);
 	else if (k == SLEEPING && life->status == CONTINUE)
-		printf("%zu: Philosopher %i is sleeping\n", time, philo->id);
+		printf("%zu: %i is sleeping\n", time, philo->id);
 	else if (k == THINKING && life->status == CONTINUE)
-		printf("%zu: Philosopher %i is thinking\n", time, philo->id);
+		printf("%zu: %i is thinking\n", time, philo->id);
 	else if (k == DEAD)
-		printf("\033[91m%zu: Philosopher %i is dead\033[0m\n", time, philo->id);
+		printf("\033[91m%zu: %i is dead\033[0m\n", time, philo->id);
 	pthread_mutex_unlock(&life->print);
 }
